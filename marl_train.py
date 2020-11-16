cea1ca3626f510c144b0728311ad5f0d7db74f0c
Timothy <timothy.bellaiche@gmail.com>
2020-11-16 21:08:45 +0100

    added gradients in wandb log

--- marl_train.py
+++ marl_train.py
@@ -35,7 +35,7 @@ def train_agent(
 
         # Models to watch metrics from
         if trained_agent.model != None:
-            wandb.watch(trained_agent.model)
+            wandb.watch(trained_agent.model, log='all', log_freq=10)
         
         # TODO - add other models when added
         
