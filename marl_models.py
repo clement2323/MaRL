db89d6921b713c50cfae9fd04c10dacaa29bb923
clement <guillo.clement@gmail.com>
2020-11-24 22:20:05 +0100

    more layers on small conv

--- marl_models.py
+++ marl_models.py
@@ -66,8 +66,8 @@ class ConvModel_small_output(nn.Module):
     def __init__(self,  n_actions):
         super(ConvModel_small_output, self).__init__()
         self.n_actions=n_actions
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32,8, kernel_size=3)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3)
+        self.conv2 = nn.Conv2d(64,8, kernel_size=3)
         self.fc1 = nn.Linear(72, n_actions)
        
 
